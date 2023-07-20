8f5f7c64a05009ea7dee421ea4b9451b759c29dd
Byron <byrondavid87@gmail.com>
2023-07-20 14:14:44 -0700

    Added images from folder and added image enlargement. Added Hallmark to page. Added jump links.

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+sony_folder = os.listdir('static/sony/')
+sony_folder.sort()
+
+hallmark_folder = os.listdir('static/hallmark/')
+hallmark_folder.sort()
+
+personal_folder = os.listdir('static/personal/')
+personal_folder.sort()
+
 @app.route("/")
 def index():
-    return render_template("layout.html")
+    return render_template("layout.html", sony_folder=sony_folder, hallmark_folder=hallmark_folder, personal_folder=personal_folder)
 
 # @app.route("/sony")
 # def index():
-#     return render_template("sony.html")
+#     return render_template('sony.html')
 
 # @app.route("/hallmark")
 # def index():
@@ -29,5 +39,5 @@ def index():
 # def index():
 #     return render_template("contact.html")
 
-if __name__== '__main__':
-    app.run(debug=True)
+# if __name__== '__main__':
+#     app.run(debug=True)
---
+++ static/sony.js
@@ -0,0 +1,20 @@
+const imgs = document.querySelectorAll('.gallery_li img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+});
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("nav");
+var link = header.getElementsByClassName("link");
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
