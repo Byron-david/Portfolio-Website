c43163977c48572901f9e163171549e2c2379af5
Byron <byrondavid87@gmail.com>
2024-03-19 17:05:40 -0700

    Added google videos I did for different papers.

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import os
 
 app = Flask(__name__)
 
+google_video = os.listdir('static/google/minitaur')
+
+google_folder = os.listdir('static/google/')
+google_folder.sort()
+
 sony_folder = os.listdir('static/sony/')
 sony_folder.sort()
 
@@ -14,4 +19,9 @@ personal_folder.sort()
 
 @app.route("/")
 def index():
-    return render_template("layout.html", sony_folder=sony_folder, hallmark_folder=hallmark_folder, personal_folder=personal_folder)
+    return render_template("layout.html", 
+                           google_folder=google_folder,
+                           google_video=google_video,
+                           sony_folder=sony_folder, 
+                           hallmark_folder=hallmark_folder, 
+                           personal_folder=personal_folder)
